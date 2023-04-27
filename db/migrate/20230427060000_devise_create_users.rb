cb65cea078dc758c35dd63ba3106a296170cd1f4
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-04-27 15:08:12 +0900

    userモデルにカラム追加

--- db/migrate/20230427060000_devise_create_users.rb
+++ db/migrate/20230427060000_devise_create_users.rb
@@ -6,10 +6,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,            null: false
+      t.string :last_name_kanji,     null: false
+      t.string :first_name_kanji,    null: false
+      t.string :last_name_kana,      null: false
+      t.string :first_name_kana,     null: false
+      t.date   :birth_date,          null: false
+ 
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
+
 
       ## Rememberable
       t.datetime :remember_created_at
