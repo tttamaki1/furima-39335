155c0c909cd2ccb42c8ed2e618c0cbcc86eb1087
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-04-29 15:25:52 +0900

    出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true
@@ -8,6 +9,7 @@ class Item < ApplicationRecord
   validates :shipping_estimate_id, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
+  extend ActiveHash::Associations::ActiveRecordExtensions 
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee_bearer
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_29_041036) do
+ActiveRecord::Schema.define(version: 2023_04_29_052947) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2023_04_29_041036) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
