327146889f2c637ba41db789dabfe7e2ff02872b
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-05-01 20:42:57 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :redirect_if_sold, only: [:edit, :update]
+  # before_action :redirect_if_sold, only: [:edit, :update]
   before_action :redirect_unless_owner, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: :DESC)
   end
@@ -20,32 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find_by(id: params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
       render 'edit'
-    end
-  end
-
-  def redirect_if_sold
-    @item = Item.find(params[:id])
-    if PurchaseHistory.exists?(item_id: @item.id)
-      redirect_to root_path
-    end
-  end
-
-  def redirect_unless_owner
-    unless current_user == @item.user
-      redirect_to root_path
     end
   end
 
@@ -54,6 +39,23 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :price, :condition_id, :category_id, :shipping_fee_bearer_id,
                                  :prefecture_id, :shipping_estimate_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end  
+
+  # def redirect_if_sold
+  #   @item = Item.find(params[:id])
+  #   if PurchaseHistory.exists?(item_id: @item.id)
+  #     redirect_to root_path
+  #   end
+  # end
+
+  def redirect_unless_owner
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
 end
--- db/migrate/20230501090126_create_purchase_histories.rb
+++ db/migrate/20230501090126_create_purchase_histories.rb
@@ -1,8 +1,8 @@
 class CreatePurchaseHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :purchase_histories do |t|
-      t.references :user ,null: false, foreign_key: true
-      t.references :item ,null: false, foreign_key: true
+      #t.references :user ,null: false, foreign_key: true
+      #t.references :item ,null: false, foreign_key: true
       t.timestamps
     end
   end
