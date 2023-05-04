098317f5f7adc3783f9522ccb03fd71dd8b7872c
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-05-04 18:42:23 +0900

    修正1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :redirect_if_sold, only: [:new, :create]
   before_action :redirect_if_owner, only: [:new, :create]
-  # before_action :set_item, only: [:new, :create]
 
   def new
     @item = Item.find(params[:id])
@@ -46,6 +45,7 @@ class OrdersController < ApplicationController
   end
 
   def redirect_if_owner
+    @item = Item.find(params[:id])
     return if current_user != @item.user
 
     redirect_to root_path
