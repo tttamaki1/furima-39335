86aa1f00f92ea339630e9d9978377d7fa24e71d1
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-05-05 11:25:10 +0900

    エラーメッセージの日本語化

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'rubocop', require: false
 
 gem 'pry-rails'
 
+gem 'rails-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -5,11 +5,11 @@ class OrderForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'をハイフンを含めて入力してください。' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number', allow_blank: true }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は数字のみ入力してください', allow_blank: true }
     validates :token
   end
 
@@ -17,5 +17,9 @@ class OrderForm
     purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address,
                            building_name: building_name, phone_number: phone_number, purchase_history_id: purchase_history.id)
+  end
+
+  def self.human_attribute_name(attr, options = {})
+    I18n.t("models.order_form.#{attr}", default: super)
   end
 end
